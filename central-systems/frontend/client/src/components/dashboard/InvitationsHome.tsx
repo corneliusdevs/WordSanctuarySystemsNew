0d63199c0f6998d05f88446b7deafc2d99d8c852
Eluan96 <eluan.ea@gmail.com>
2025-01-28 15:56:04 +0100

    welcome

--- central-systems/frontend/client/src/components/dashboard/InvitationsHome.tsx
+++ central-systems/frontend/client/src/components/dashboard/InvitationsHome.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React from "react";
 import { IoAdd } from "react-icons/io5";
 import DashboardNavbar from "./DashboardNavbar";
@@ -14,11 +14,11 @@ const InvitationsHome = () => {
     <div>
       <DashboardNavbar />
       <DashboardWelcomeComponent username={""} title={""} />
-      <div className="mt-10">
-        <Link href='/dashboard/home'>
-        <IoArrowBack className="h-10 w-20" />
+      <div className="mt-10 px-">
+        <Link href="/dashboard/home">
+          <IoArrowBack className="h-10 w-20" />
         </Link>
-        <h1 className="text-5xl text-center font-bold text-[#3A2D4A]">
+        <h1 className="text-4xl text-center font-bold text-[#3A2D4A]">
           Invitations
         </h1>
         <h2 className="text-2xl text-[#3A2D4A] ml-5 mt-5 font-bold">
@@ -26,9 +26,9 @@ const InvitationsHome = () => {
         </h2>
         <div className="flex items-center justify-center py-10">
           <Link href="/dashboard/invitations/create-invite">
-          <div className="flex items-center justify-center h-18 w-24 rounded-3xl border-4 border-[#3A2D4A] ">
-            <IoAdd className="h-14 w-14 text-[#3A2D4A] font-bold" />
-          </div>
+            <div className="flex items-center justify-center h-18 w-24 rounded-3xl border-4 border-[#3A2D4A] ">
+              <IoAdd className="h-14 w-14 text-[#3A2D4A] font-bold" />
+            </div>
           </Link>
         </div>
       </div>
